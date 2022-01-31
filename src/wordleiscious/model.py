19c0558a915ef5bc43c4bf7d52fbfa5c0a3fb2e2
richard.harris <richard.harris@oodlefinance.com>
2022-01-31 04:05:36 +0000

    Vectorised.

--- src/wordleiscious/model.py
+++ src/wordleiscious/model.py
@@ -1,170 +1,199 @@
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
+from typing import Dict, List, Iterable
 
-from wordleiscious.words import all_words, weights
+from wordleiscious.words import candidate_weights, answers
 
 
 class Solver:
-    def __init__(self, weighted_candidate_df: pd.DataFrame, allowed_guess: pd.Series):
-        self.weighted_candidate_df = weighted_candidate_df
+    def __init__(self, candidate_weight: pd.Series, allowed_guess: pd.Series):
+        self.candidate_weight = candidate_weight
         self.allowed_guess = allowed_guess
+
+    @classmethod
+    def from_weights_and_guesses(
+        cls, candidate_weights: Dict[str, float], allowed_guesses: Iterable[str]
+    ):
+
+        return cls(
+            candidate_weight=pd.Series(name="weight", data=candidate_weights),
+            allowed_guess=pd.Series(name="guess", data=allowed_guesses),
+        )
 
     def with_guess(self, guess: str, outcome: str) -> "Solver":
         remaining = Solver.remaining(
-            candidate=self.weighted_candidate_df.index.to_series(),
-            guess=guess,
-            outcome=outcome,
+            candidate=self.candidate_weight.index.to_series(),
+            guess=pd.Series(
+                index=self.candidate_weight.index, data=guess, name="guess"
+            ),
+            outcome=pd.Series(
+                index=self.candidate_weight.index, data=outcome, name="outcome"
+            ),
         )
-        weighted_candidate_df = self.weighted_candidate_df[remaining].copy()
+        candidate_weight = self.candidate_weight[remaining].copy()
         allowed_guess = self.allowed_guess[self.allowed_guess != guess].copy()
-        return Solver(
-            weighted_candidate_df=weighted_candidate_df, allowed_guess=allowed_guess
-        )
+        return Solver(candidate_weight=candidate_weight, allowed_guess=allowed_guess)
 
     @staticmethod
-    def remaining(candidate: pd.Series, guess: str, outcome: str) -> pd.Series:
-        n = len(outcome)
-        assert len(guess) == n, f"len(guess)({len(guess)}) != len(outcome)({n})"
+    def remaining(
+        candidate: pd.Series, guess: pd.Series, outcome: pd.Series
+    ) -> pd.Series:
+        n = candidate.str.len().max()
 
         remaining = pd.Series(index=candidate.index, name="remaining", data=True)
 
         remaining &= remaining[candidate != guess]
 
-        for i, (g, o) in enumerate(zip(guess, outcome)):
-            if o == "🟩":
-                remaining &= candidate.str[i] == g
-            elif o == "🟨":
-                remaining &= candidate.str.contains(g)
-            elif o == "🟦":
-                remaining &= ~candidate.str.contains(g)
+        cs = [candidate.str[i] for i in range(n)]
+
+        for i in range(n):
+            c = candidate.str[i]
+            o = outcome.str[i]
+            g = guess.str[i]
+
+            green_mask = o == "🟩"
+            yellow_mask = o == "🟨"
+            black_mask = o == "🟦"
+
+            remaining[green_mask] &= (c == g)[green_mask]
+
+            has_g = pd.Series(index=candidate.index, data=False)
+            for ii in range(n):
+                has_g |= cs[ii] == g
+            remaining[yellow_mask] &= has_g[yellow_mask]
+
+            remaining[black_mask] &= ~has_g[black_mask]
 
         return remaining
 
     @staticmethod
-    def outcome_after_guess(solution: pd.Series, guess: str) -> pd.Series:
+    def outcome_after_guess(candidate: pd.Series, guess: pd.Series) -> pd.DataFrame:
 
-        outcome = pd.Series(name="outcome", index=solution.index, data="")
+        n = candidate.str.len().max()
 
-        for i, g in enumerate(guess):
-            green_mask = solution.str[i] == g
-            yellow_mask = ~green_mask & solution.str.contains(g)
+        outcome_df = pd.merge(candidate, guess, how="cross")
+
+        outcome_df["outcome"] = ""
+
+        for i in range(n):
+
+            green_mask = outcome_df.guess.str[i] == outcome_df.candidate.str[i]
+
+            yellow_mask = pd.Series(index=outcome_df.index, data=False)
+            for ii in range(n):
+                yellow_mask |= outcome_df.guess.str[i] == outcome_df.candidate.str[ii]
+            yellow_mask &= ~green_mask
+
             black_mask = ~green_mask & ~yellow_mask
 
-            outcome[green_mask] += "🟩"
-            outcome[yellow_mask] += "🟨"
-            outcome[black_mask] += "🟦"
+            outcome_df.loc[green_mask, "outcome"] += "🟩"
+            outcome_df.loc[yellow_mask, "outcome"] += "🟨"
+            outcome_df.loc[black_mask, "outcome"] += "🟦"
 
-        return outcome
+        return outcome_df
 
     @staticmethod
-    def scalar_outcome_after_guess(solution: str, guess: str) -> str:
-        solution_series = pd.Series(data=solution, name="candidate")
-        outcome = Solver.outcome_after_guess(
-            solution=pd.Series(data=solution_series, name="candidate"), guess=guess
+    def scalar_outcome_after_guess(candidate: str, guess: str) -> str:
+        single_candidate = pd.Series(data=[candidate], name="candidate")
+        single_guess = pd.Series(data=[guess], name="guess")
+        outcome_df = Solver.outcome_after_guess(
+            candidate=single_candidate, guess=single_guess
         )
-        return outcome.values[0]
+        return outcome_df.outcome.values[0]
 
-    def evaluate_guess(self, guess: str) -> float:
+    def evaluate_guesses(self) -> pd.Series:
 
-        outcome = Solver.outcome_after_guess(
-            solution=self.weighted_candidate_df.index.to_series(), guess=guess
-        )
-
-        outcome_counts = outcome.value_counts()
-
-        unique_outcomes, weights = zip(*outcome_counts.iteritems())
-
-        fractions_remaining = [
-            (
-                Solver.remaining(
-                    candidate=self.weighted_candidate_df.index.to_series(),
-                    guess=guess,
-                    outcome=unique_outcome,
-                )
-                * self.weighted_candidate_df.frequency
-            ).mean()
-            for unique_outcome in unique_outcomes
-        ]
-
-        return np.average(a=fractions_remaining, weights=weights)
-
-    def evaluate_guesses(self) -> pd.DataFrame:
-        return pd.DataFrame.from_dict(
-            {
-                guess: {"fraction_remaining": self.evaluate_guess(guess=guess)}
-                for guess in tqdm(self.allowed_guess, leave=False)
-            },
-            orient="index",
+        outcome_df = Solver.outcome_after_guess(
+            candidate=self.candidate_weight.index.to_series().rename("candidate"),
+            guess=self.allowed_guess,
         )
 
-    def best_guesses(self):
-        guess_df = self.evaluate_guesses()
-        best_guess_fraction = guess_df.fraction_remaining.min()
-        return guess_df[guess_df.fraction_remaining == best_guess_fraction].copy()
+        unique_candidate_and_outcome_counts_df = (
+            outcome_df.groupby(["guess", "outcome"])
+            .size()
+            .rename("count")
+            .reset_index()
+        )
+
+        c_df = self.candidate_weight.reset_index().rename(
+            columns={"index": "candidate"}
+        )
+
+        weighted_outcome_df = unique_candidate_and_outcome_counts_df.merge(
+            c_df, how="cross"
+        )
+
+        weighted_outcome_df["remaining"] = Solver.remaining(
+            candidate=weighted_outcome_df.candidate,
+            outcome=weighted_outcome_df.outcome,
+            guess=weighted_outcome_df.guess,
+        )
+
+        weighted_outcome_df["guess_score_contribution"] = (
+            weighted_outcome_df["count"]
+            * weighted_outcome_df.weight
+            * (1 - weighted_outcome_df.remaining.astype(float))
+        )
+
+        return (
+            weighted_outcome_df.groupby("guess")
+            .guess_score_contribution.sum()
+            .rename("score")
+        )
+
+    def best_guesses(self) -> pd.DataFrame:
+        guess_scores_df = self.evaluate_guesses().reset_index()
+        best_guess_score = guess_scores_df.score.max()
+        return guess_scores_df[guess_scores_df.score == best_guess_score]
 
     def a_best_guess(self) -> str:
-        if self.weighted_candidate_df.index.size == 1:
-            return self.weighted_candidate_df.index.values[0]
-        return self.best_guesses().sample().index.values[0]
+        if self.candidate_weight.index.size == 1:
+            return self.candidate_weight.index.values[0]
+        return self.best_guesses().sample().guess.values[0]
 
 
-# def emojify(outcome: str):
-#     return outcome.replace("g", "🟩").replace("y", "🟨").replace("b", "🟦")
+def main():
 
+    c_weights = candidate_weights()
 
-def main2():
+    def _pre_display(guess: str, outcome: str):
+        print(f"guess:'{guess}', outcome:{outcome}", end="")
 
-    ft_df = weights()
-    candidate_index = pd.Index(name="word", data=list(all_words()))
+    def _post_display(s:Solver):
+        print(f", remaining candidates:{s.candidate_weight.index.size}")
 
-    weighted_candidate_df = (
-        ft_df.reindex(candidate_index)
-        .fillna(ft_df.weight.min())
-        .sort_values("weight", ascending=False)
-    )
+    for solution in answers():
 
-    weighted_candidate_df.weight /= weighted_candidate_df.weight.sum()
-
-    solution = "perky"
-    # first_guess = "raise"
-
-    # outcome = Solver.scalar_outcome_after_guess(solution=solution, guess=first_guess)
-
-    # print(f"guess:'{first_guess}', outcome:{outcome}", end="")
-    # s = Solver.with_first_guess(
-    #     weighted_candidate_df=weighted_candidate_df,
-    #     allowed_guess=weighted_candidate_df.index.to_series(),
-    #     guess=first_guess,
-    #     outcome=outcome,
-    # )
-    # print(f", remaining candidates:{s.weighted_candidate_df.index.size}")
-
-    s = Solver(
-        weighted_candidate_df=weighted_candidate_df,
-        allowed_guess=weighted_candidate_df.index.to_series(),
-    )
-
-    while True:
-        best_guess = s.a_best_guess()
-
-        best_outcome = Solver.scalar_outcome_after_guess(
-            solution=solution, guess=best_guess
+        first_guess = "tares"
+        first_outcome = Solver.scalar_outcome_after_guess(
+            candidate=solution, guess=first_guess
         )
-        print(f"guess:'{best_guess}', outcome:{best_outcome}", end="")
-        if best_outcome == "🟩🟩🟩🟩🟩":
-            break
-        s = s.with_guess(guess=best_guess, outcome=best_outcome)
-        print(f", remaining candidates:{s.weighted_candidate_df.index.size}")
+
+        _pre_display(guess=first_guess, outcome=first_outcome)
+        s = Solver.from_weights_and_guesses(
+            candidate_weights=c_weights, allowed_guesses=c_weights.keys()
+        ).with_guess(
+            guess=first_guess,
+            outcome=Solver.scalar_outcome_after_guess(
+                candidate=solution, guess=first_guess
+            ),
+        )
+        _post_display(s=s)
+
+        while True:
+            best_guess = s.a_best_guess()
+
+            best_outcome = Solver.scalar_outcome_after_guess(
+                candidate=solution, guess=best_guess
+            )
+            _pre_display(guess=best_guess, outcome=best_outcome)
+            if best_outcome == "🟩🟩🟩🟩🟩":
+                break
+            s = s.with_guess(guess=best_guess, outcome=best_outcome)
+            _post_display(s=s)
+
+        print("\n" * 2)
 
 
 if __name__ == "__main__":
-    main2()
-
-
-# guess:'raise', outcome:🟨🟦🟦🟦🟨, remaining candidates:602
-# guess:'donut', outcome:🟦🟦🟦🟦🟦, remaining candidates:128
-# guess:'glyph', outcome:🟦🟦🟨🟨🟦, remaining candidates:10
-# guess:'remex', outcome:🟨🟩🟦🟨🟦, remaining candidates:4
-# guess:'kieve', outcome:🟨🟦🟨🟦🟨, remaining candidates:1
-# guess:'perky', outcome:🟩🟩🟩🟩🟩
+    main()
--- src/wordleiscious/words.py
+++ src/wordleiscious/words.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Dict
 from importlib import resources
 import json
 import pandas as pd
@@ -21,6 +21,30 @@ def all_words() -> Iterable[str]:
     yield from allowed_guesses()
 
 
-def weights() -> pd.DataFrame:
-    with resources.open_text(DATA_PACKAGE, "weights.json.gzip") as path_or_buf:
-        return pd.read_json(path_or_buf=path_or_buf)
+def word_frequency() -> pd.DataFrame:
+    return pd.read_feather(path=resources.path(DATA_PACKAGE, "word_frequency.feather"))
+
+
+def process_frequency_file():
+    with resources.open_text(DATA_PACKAGE, "unigram_freq.csv") as filepath_or_buffer:
+        unigram_freq_df = pd.read_csv(filepath_or_buffer=filepath_or_buffer)
+    unigram_freq_df = unigram_freq_df[unigram_freq_df.word.str.len() == 5].reset_index(
+        drop=True
+    )
+    unigram_freq_df.to_feather(
+        path=resources.path(DATA_PACKAGE, "word_frequency.feather")
+    )
+
+
+def candidate_weights() -> Dict[str, float]:
+    frequency_df = word_frequency().set_index("word")
+    word_index = pd.Index(data=list(all_words()))
+
+    weighted_candidate_df = (
+        frequency_df.reindex(word_index)
+            .fillna(frequency_df["count"].min())
+            .rename(columns={"count": "weight"})
+    )
+    weighted_candidate_df["weight"] /= weighted_candidate_df["weight"].sum()
+
+    return weighted_candidate_df.weight.to_dict()
